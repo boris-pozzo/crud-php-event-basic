6354d642c9590f86c1da104b829c485f62e79d4e
Evinym <Evinym@outlook.fr>
2018-04-09 12:50:24 -0400

    To completed

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$host= "localhost";
+$username= "root";
+$password= "sexymoi2580";
+$dbname= "test";
+$dsn= "mysql:host=$host;dbname=$dbname";
+$options= array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+);
+
+
+?>
--- public/event.php
+++ public/event.php
@@ -1,32 +1,66 @@
 <?php
+
 require "functions.php";
+
+if(isset($_GET['id'])){
+	list($id, $name, $surname, $passwordd) = get_event(filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT));
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){ 
+	$id=filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+	$name = trim(filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING));
+	$surname = trim(filter_input(INPUT_POST, 'surname', FILTER_SANITIZE_STRING));
+	$passwordd = trim(filter_input(INPUT_POST, 'passwordd', FILTER_SANITIZE_STRING));
+
+	if(empty($name) ||empty($surname) || empty($passwordd)){
+		$error_message= "Please fill in the required fields";
+	} else {
+
+		if(add_event($name, $surname, $passwordd, $id)){
+			header('Location: index.php');
+			exit;
+		} else {
+			$error_message = "Could not add account";
+		}
+	}
+}
+
 ?>
 
 <?php require "templates/header.php"; ?>
 
-<h2>Add an event</h2>
+<?php 
+	if(isset($error_message)){
+		echo $error_message;
+	}
+?>
+
+<h2>
+	<?php
+		if(!empty($id)){
+			echo "Update";
+		} else {
+			echo "Create your acount";
+		}
+	?>
+</h2>
 
 <form method="post" action="event.php">
-	<label for="name">Event name</label>
+
+	<label for="name">name</label>
 	<input type="text" name="name" id="name" value="">
 
-	<label for="date">Date</label>
-	<input type="date" name="date" id="date" value="">
+	<label for="surname">surname</label>
+	<input type="text" name="surname" id="surname" value="">
 
-	<label for="start">Start</label>
-	<input type="time" name="start" id="start" value="">
+	<label for="passwordd">choose your password</label>
+	<input type="password" name="passwordd" id="passwordd" value="">
 
-	<label for="end">End</label>
-	<input type="time" name="end" id="end" value="">
-
-	<label for="description">Description</label>
-    <textarea id="description" name="description"></textarea>
-
-	<label for="price">Price</label>
-	<input type="text" name="price" id="price" value="">
-
-	<label for="image">Image</label>
-	<input type="text" name="image" id="image" value="">
+	<?php
+		if(!empty($id)){
+			echo '<input type="hidden" name="id" value="'.$id.'">';
+		}
+	?>
 
 	<input type="submit" name="submit" value="Envoyer">
 </form>
